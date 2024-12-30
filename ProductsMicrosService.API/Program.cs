d6a4f8290b0eb10daa411e9e74c407e263c89873
Mutala Mutala <jonamut12.live@gmail.com>
2024-12-30 01:00:40 -0500

    Add Middleware

--- ProductsMicrosService.API/Program.cs
+++ ProductsMicrosService.API/Program.cs
@@ -1,6 +1,29 @@
+using BusinessLogicLayer;
+using DataAccesLayer;
+using FluentValidation.AspNetCore;
+using ProductsMicrosService.API.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddBusinessLogicLayer();
+builder.Services.AddDataAccessLayer();
+
+
+builder.Services.AddControllers();
+
+
+// FluentValidations 
+builder.Services.AddFluentValidationAutoValidation(); 
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+app.UseAuthentication(); 
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
