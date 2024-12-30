aaea83d142c65d23f416c21b15949926974eea74
Mutala Mutala <jonamut12.live@gmail.com>
2024-12-30 01:00:25 -0500

    Add Business and DataAccess Layer

---
+++ DataAccesLayer/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer
+{
+    public static class DependencyInjection
+    {
+        public static  IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+        {
+            //TO DO : Add Data access Layer services 
+
+
+            return services; 
+        }
+    }
+}
